261b2c9127ba9b5d4fd5eb5cd45ec72604345f2e
yasu22go25 <kyasu8917@gmail.com>
2021-04-14 16:55:55 +0900

    UserId作成時にUUIDを生成

---
+++ src/main/kotlin/com/yasu22go25/twitterclone/controller/dto/response/UserResponse.kt
@@ -0,0 +1,3 @@
+package com.yasu22go25.twitterclone.controller.dto.response
+
+data class UserResponse()
--- src/main/kotlin/com/yasu22go25/twitterclone/domain/value/UserId.kt
+++ src/main/kotlin/com/yasu22go25/twitterclone/domain/value/UserId.kt
@@ -1,5 +1,10 @@
 package com.yasu22go25.twitterclone.domain.value
 
+import java.util.*
+
 data class UserId(
     val value: String
-)
+) {
+    constructor(): this(UUID.randomUUID().toString())
+    constructor(id: String): this(id)
+}
