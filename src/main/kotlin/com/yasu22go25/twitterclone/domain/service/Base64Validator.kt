fd419ddf2211640bf6eba39893926c0b21615ac1
yasu22go25 <kyasu8917@gmail.com>
2021-04-15 18:34:13 +0900

    base64フォーマットの検証クラスを作成

---
+++ src/main/kotlin/com/yasu22go25/twitterclone/domain/service/Base64Validator.kt
@@ -0,0 +1,13 @@
+package com.yasu22go25.twitterclone.domain.service
+
+import org.springframework.stereotype.Component
+
+@Component
+class Base64Validator {
+
+    fun validate(base64: String): Boolean {
+        val regex = Regex("data:.*/.*;base64,.*")
+        return regex.containsMatchIn(base64)
+    }
+
+}
---
+++ src/test/kotlin/com/yasu22go25/twitterclone/domain/service/Base64ValidatorTests.kt
@@ -0,0 +1,53 @@
+package com.yasu22go25.twitterclone.domain.service
+
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.`is`
+
+@SpringBootTest
+class Base64ValidatorTests {
+
+    val validator = Base64Validator()
+
+    @Test
+    @DisplayName("imageメディアでパターンマッチする時、True")
+    fun testNormalForImage() {
+        val expected = true
+        val text = "data:image/jpeg;base64,AAAAAAAABBBBBBBBCCCCCCCCDDDDDDDDEEEEEEEEFFFFFFFFGGGGGGGGG"
+        val actual = validator.validate(text)
+
+        assertThat(actual, `is`(expected))
+    }
+
+    @Test
+    @DisplayName("dataが無い時、False")
+    fun testFailureForNoData() {
+        val expected = false
+        val text = "image/jpeg;base64,AAAAAAAABBBBBBBBCCCCCCCCDDDDDDDDEEEEEEEEFFFFFFFFGGGGGGGGG"
+        val actual = validator.validate(text)
+
+        assertThat(actual, `is`(expected))
+    }
+
+    @Test
+    @DisplayName("MimeTypeが無い時、False")
+    fun testFailureForNoMimeType() {
+        val expected = false
+        val text = "data:;base64,AAAAAAAABBBBBBBBCCCCCCCCDDDDDDDDEEEEEEEEFFFFFFFFGGGGGGGGG"
+        val actual = validator.validate(text)
+
+        assertThat(actual, `is`(expected))
+    }
+
+    @Test
+    @DisplayName("base64が無い時、False")
+    fun testFailureForNoBase64() {
+        val expected = false
+        val text = "data:image/jpeg;AAAAAAAABBBBBBBBCCCCCCCCDDDDDDDDEEEEEEEEFFFFFFFFGGGGGGGGG"
+        val actual = validator.validate(text)
+
+        assertThat(actual, `is`(expected))
+    }
+}
